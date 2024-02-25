b4caec6537556c77d432f24e0b1cfb3f485ea920
prinnasit <prinnasit@gmail.com>
2024-02-25 18:11:51 +0700

    monster_01

---
+++ src/Monsters/Base_Monster.java
@@ -0,0 +1,14 @@
+package Monsters;
+
+
+public class Base_Monster extends Monster {
+    public Base_Monster(int maxHP, int atk, int def , int magicatk , int magicdef) {
+        super(maxHP, atk, def , magicatk ,magicdef);
+    }
+
+   @Override
+   public void attack(Monster m) {
+       //to do//
+    }
+
+}
---
+++ src/Monsters/Tank_Monster.java
@@ -0,0 +1,12 @@
+package Monsters;
+
+public class Tank_Monster extends Monster {
+
+    public Tank_Monster(int maxHP, int atk, int def , int magicatk , int magicdef) {
+        super(maxHP, atk, def , magicatk ,magicdef);
+    }
+
+    public void attack(Monster m) {
+        //todo
+    }
+}
