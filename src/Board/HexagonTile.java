88e02331e3a582cc5237d21d813efee752e9192c
aiyapatv <6633288721@student.chula.ac.th>
2024-02-29 12:52:44 +0700

    stash

--- src/Board/HexagonTile.java
+++ src/Board/HexagonTile.java
@@ -8,11 +8,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
 public class HexagonTile extends StackPane {
-    private int number;
+    private int q;
+    private int r;
     private boolean isWalked;
 
-    public HexagonTile(int number, boolean isWalked, String image, int size){
-        setNumber(number);
+    public HexagonTile(int q, int r,boolean isWalked, String image, int size){
+        setQ(q);
+        setR(r);
         setIsWalked(false);
         createHexagon(image, size);
     }
@@ -20,7 +22,7 @@ public class HexagonTile extends StackPane {
     public void createHexagon(String image, int size){
         ImageView hexagonImage = Images.setImageViewSize(ToolKit.loadImage(image), size, size);
         getChildren().add(hexagonImage);
-        getChildren().add(new Label(Integer.toString(number)));
+        getChildren().add(new Label(Integer.toString((Math.abs(q) + Math.abs(-q-r) + Math.abs(-r))/2)));
         setOnMouseEntered(mouseEvent -> {
             hexagonImage.setEffect(new Glow(0.5));
         });
@@ -29,12 +31,20 @@ public class HexagonTile extends StackPane {
         });
     }
 
-    public int getNumber() {
-        return number;
+    public int getQ() {
+        return q;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public void setQ(int q) {
+        this.q = q;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
     }
 
     public boolean isWalked() {
